e8ecbd1478dfb71bff9bbd1d3d9577c4cce38360
Matteo Lopez <matteolopezbb@gmail.com>
2022-04-19 15:15:34 +0200

    creazione del fil php, con inserimento di un paragrafo

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    $paragraph = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem, consequuntur tenetur! Dignissimos repellat iste deleniti sequi veritatis. Quo quas mollitia voluptates eos exercitationem, earum, possimus aut saepe harum sunt rem.';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my First php test</title>
+</head>
+<body>
+    <p>
+        <?php echo $paragraph ?>
+    </p>
+</body>
+</html>
